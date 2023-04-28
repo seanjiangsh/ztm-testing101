8ef860a40e53a748b55abfc100230a052f399278
sean <seanjiangsh@gmail.com>
2023-04-28 19:38:01 +0800

    Added mocked fetch and corresponding async test

--- src/asyncScript.test.ts
+++ src/asyncScript.test.ts
@@ -4,8 +4,19 @@ describe("async function", () => {
   test("calls swapi to get people", async () => {
     // * assert has 2 "assertions(expect)" to be called
     expect.assertions(2);
-
-    const result = await getPeople();
+    const result = await getPeople(fetch);
+    expect(result?.count).toEqual(87);
+    expect(result?.results.length).toBeGreaterThan(5);
+  });
+  test("get people with mocked fetch", async () => {
+    // * assert has 2 "assertions(expect)" to be called
+    expect.assertions(4);
+    const mockResult = { count: 87, results: [0, 1, 2, 3, 4, 5] };
+    const mockJson = { ok: true, json: () => Promise.resolve(mockResult) };
+    const mockFetch = jest.fn().mockReturnValue(Promise.resolve(mockJson));
+    const result = await getPeople(mockFetch);
+    expect(mockFetch.mock.calls.length).toBe(1);
+    expect(mockFetch).toBeCalledWith("https://swapi.py4e.com/api/people");
     expect(result?.count).toEqual(87);
     expect(result?.results.length).toBeGreaterThan(5);
   });
--- src/asyncScript.ts
+++ src/asyncScript.ts
@@ -1,5 +1,9 @@
-export const getPeople = async () => {
-  const url = "https://swapi.py4e.com/api/people/";
+type Fetch = (
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+) => Promise<Response>;
+export const getPeople = async (fetch: Fetch) => {
+  const url = "https://swapi.py4e.com/api/people";
   const fetchResult = await fetch(url);
   if (!fetchResult.ok) return;
   const json = await fetchResult.json();
